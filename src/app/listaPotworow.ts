ce23fa73d6ed9c5d73bdaaf16d3b7ab011c0bcf7
Dominika Zając <dominikaz@qualtrics.com>
2020-05-31 15:56:03 +0200

    change assets addresses for stackblitz

--- src/app/listaPotworow.ts
+++ src/app/listaPotworow.ts
@@ -1,22 +1,22 @@
 export const listaPotworow = [
     {
       name: "Ania",
-      icon: "../assets/c1.png"
+      icon: "https://github.com/dominika-zajac/for-example/blob/master/src/assets/c1.png?raw=true"
     },
     {
       name: "Janek",
-      icon: "../assets/c2.png"
+      icon: "https://github.com/dominika-zajac/for-example/blob/master/src/assets/c2.png?raw=true"
     },
     {
       name: "Grześ",
-      icon: "../assets/c3.png",
+      icon: "https://github.com/dominika-zajac/for-example/blob/master/src/assets/c3.png?raw=true",
     },
     {
       name: "Zosia",
-      icon: "../assets/c4.png",
+      icon: "https://github.com/dominika-zajac/for-example/blob/master/src/assets/c4.png?raw=true",
     },
     {
       name: "Bartek",
-      icon: "../assets/c5.png",
+      icon: "https://github.com/dominika-zajac/for-example/blob/master/src/assets/c5.png?raw=true",
     }
   ]
